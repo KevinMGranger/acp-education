913ca901ad5b04ab96be72b278014ee2fb74f13a
Kevin Granger <kmg2728@rit.edu>
2013-02-13 16:04:48 -0500

    init commit

---
+++ edusim.m
@@ -0,0 +1,20 @@
+function [] = edusim (students,)
+%
+% DESCRIPTION
+% 
+% 
+% 
+% 
+% RETURNS
+% 
+% 
+% 
+% 
+% ARGUMENTS
+% 
+% 
+% 
+% AUTHOR
+%     Kevin Granger
+%     kmg2728@rit.edu
+%     2013-02-21
